e6aaa5d82853fa4f0619ceb50f32dcfbf4f28349
Megan Nowak <MNowak16@my.wctc.edu>
2019-10-14 19:51:30 -0500

    Changes required for Midterm

---
+++ RaceTrack/Cars/FormulaOne.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class FormulaOne : RaceCar
+    {
+        public FormulaOne()
+        {
+            Name = "Formula One";
+            TopSpeed = 230;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} sputters and turns over!");
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} spins out of control and crashes through the finish line!");
+            base.Brake();
+        }
+
+    }
+}
---
+++ RaceTrack/Drivers/Megan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RaceTrack.RaceTrack.Cars;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    public class Megan : Driver
+    {
+        public Megan(RaceCar car) : base(car)
+        {
+            Name = "Megan";
+            SkillLevel = 7;
+        }
+
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+    }
+}
